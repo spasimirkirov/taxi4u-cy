8a14d7528f8dc5acb608b3ddc31cf736269b5238
Spasimir Kirov <spasimirkirov@gmail.com>
2021-12-24 15:34:30 +0200

    Base version of taxi4u-cy

---
+++ index.php
@@ -0,0 +1,389 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <title>Taxi4U | Website</title>
+    <meta name="description"
+          content="Taxi4U is a transport company providing fast and reliable transport services on Cyprus island">
+    <meta name="keywords" content="taxi, 4u, paphos, transport, cyprus, excursion, transfer, cab">
+    <meta name="author" content="Raziel Von Chronos">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS -->
+    <meta property="og:url"
+          content="http://www.taxi4u-cy.com/"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="Taxi4U | Website"/>
+    <meta property="og:description"
+          content="Taxi4U is a transport company providing fast and reliable transport services on Cyprus island"/>
+    <meta property="og:image" content="http://www.taxi4u-cy.com/style/images/logo-medium.png"/>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="style/css/bootstrap/bootstrap.min.css">
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style/css/custom.css">
+
+
+    <!-- JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="style/js/bootstrap/bootstrap.min.js"></script>
+    <script src="style/js/custom.js"></script>
+    <script src="style/js/smoothscroll.js"></script>
+</head>
+<body>
+
+<div>
+    <a class="goTop" onclick="topFunction()" title="Go to top">
+        <i class="fas fa-angle-up fa-border rounded-circle border-danger bg-light text-dark px-2 py-0"></i>
+    </a>
+</div>
+
+<div class="container-fluid bg-light">
+    <div class="row">
+        <div class="col-11 col-lg-10 offset-1">
+            <nav class="navbar navbar-expand-lg" role="navigation">
+                <div class="navbar-header mx-lg-auto">
+                    <!--Navbar logo-->
+                    <a class="navbar-brand p-0" href="./">
+                        <img src="style/images/logo-small.png" class="img-fluid" style="max-width:210px"
+                             alt="Taxi4u Logo">
+                    </a>
+                </div>
+
+                <!--Navbar in toggler-->
+                <button class="navbar-toggler float-left" type="button" data-toggle="collapse"
+                        data-target="#navbar-collapse-x"
+                        aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <!--                    <i class=" fas fa-bars fa-2x"> </i>-->
+                    <i class="fas fa-list-ul"></i>
+                </button>
+
+                <!--Navbar in full screen-->
+                <div class="d-none d-lg-block navbar-collapse text-danger">
+                    <div class="nav navbar-nav mx-lg-auto">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item"><a class="nav-item nav-link" href="./">home</a></li>
+                            <li class="nav-item"><a class="nav-item nav-link" href="#services">services</a></li>
+                            <li class="nav-item"><a class="nav-item nav-link" href="#gallery">gallery</a></li>
+                            <li class="nav-item"><a class="nav-item nav-link" href="#contact">contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!--Navbar contact for full screen-->
+                <div class="nav-contact d-none d-lg-block">
+                    <img src="style/images/cyprus_flag.png" class="img-fluid" alt="Cyprus flag" style="max-width: 120px">
+                </div>
+            </nav>
+            <!--Navbar collapse for small screen-->
+            <div class="offset-1 collapse navbar-collapse" id="navbar-collapse-x">
+                <div class="nav navbar-nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-item nav-link active" href="./">home</a></li>
+                        <li class="nav-item"><a class="nav-item nav-link" href="#services">services</a></li>
+                        <li class="nav-item"><a class="nav-item nav-link" href="#gallery">gallery</a></li>
+                        <li class="nav-item"><a class="nav-item nav-link" href="#contact">contact us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="divide col-lg-12 py-1 py-lg-2"></div>
+
+<div class="container-fluid">
+    <div class="contact row">
+        <div class="phone col-11 col-md-11 col-lg-4 offset-1 offset-sm-1 offset-lg-1">
+            <div class="col-2">
+                <i class="icon fa fa-phone-square fa-flip-horizontal float-left" aria-hidden="true"></i>
+            </div>
+            <div class="pt-lg-1 col-10">
+                <a href="tel:+35799030826">
+                    <span class="number">+35799030826</span>
+                </a>
+            </div>
+        </div>
+        <div class="location col-11 col-md-11 col-lg-3 offset-1 offset-lg-0">
+            <div class="icon col-2">
+                <i class="fas fa-map-marker-alt fa-flip-horizontal float-left"></i>
+            </div>
+            <div class="col-10 pt-lg-1">
+                <span class="city">Paphos, Cyprus </span><br>
+                <span class="workhours"><i class="far fa-clock"></i> 24/7</span>
+            </div>
+        </div>
+        <div class="social col-11 col-md-11 col-lg-4 offset-1 offset-lg-0">
+            <div class="col-12">
+                <a class="facebook" href="https://www.facebook.com/taxi4ucyprus" target="_blank">
+                    <i class="fab fa-facebook-square" aria-hidden="true"></i>
+                </a>
+                <a class="tripadvisor" href="https://www.tripadvisor.com/Profile/Taxi4UCyprus" target="_blank">
+                    <i class="fab fa-tripadvisor"></i>
+                 </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="divide col-lg-12 py-1 py-lg-2"></div>
+
+<div class="home container-fluid">
+    <div class="row">
+        <div class="col-12 col-md-5">
+            <img src="style/images/header.png" class="img-fluid mx-auto" alt="Taxi4U - Welcome to our website">
+        </div>
+        <div class="welcome col-12 col-md-7">
+            <div class="card h-100 border-0">
+                <div class="card-body mt-5">
+                    <h4 class="card-title">Welcome to <b>Cyprus</b><br>
+                        <h4 class="card-title">&#8194;Welcome to <b>Paphos</b><br>
+                            <h4 class="card-title">&#8194;&#8194;Welcome to our <b>Island</b></h4>
+                            <br>
+                            <p class="card-text">
+                                24/7 Taxi Service<br>
+                                &#8194;Airport Transfers<br>
+                                &#8194;&#8194;&#8194;Excursions all over the island<br>
+                            </p>
+                            <br>
+
+                            <ul class="p-0" style="font-size: 14pt;">
+                                <li>
+                                    <a href="tel:+35799030826">
+                                        <i class="fa fa-phone fa-flip-horizontal" aria-hidden="true"></i>+35799030826
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/taxi4ucyprus" target="_blank">
+                                        <i class="fab fa-facebook-square" aria-hidden="true"></i> fb.me/taxi4ucyprus
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="mailto:office@taxi4u-cy.com" target="_blank">
+                                        <i class="fa fa-envelope" aria-hidden="true"></i> office@taxi4u-cy.com
+                                    </a>
+                                </li>
+                            </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="services"></div>
+
+<div id="taxi" class="container-fluid">
+    <div class="row">
+        <div class="services card border w-100">
+            <div class="card-header text-center text-light rounded-0" style="background-color: #1A1C7F">
+                <h4 class="card-title p-0 m-0">Taxi services</h4>
+            </div>
+            <div class="card-body">
+                <div class="col-12 col-md-4 float-left p-2">
+                    <img class="img-fluid rounded border border-dark" src="style/images/car-side.jpg"
+                         alt="Taxi4u - Taxi services">
+                </div>
+                <div class="col-12 col-md-8 float-left p-2">
+                    <p class="card-text">For safe taxi services and transportation 24 hours a day, you can trust our
+                        professional and experienced drivers, island wide no trip too small or big with safety at hand
+                        at all times</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="transfer" class="container-fluid bg-light">
+    <div class="row">
+        <div class="services card border w-100">
+            <div class="card-header text-center text-light rounded-0" style="background-color: #1A1C7F">
+                <h4 class="card-title p-0 m-0">Airport transfers</h4>
+            </div>
+            <div class="card-body">
+                <div class="col-12 col-md-4 float-right p-2">
+                    <img class="img-fluid rounded border border-dark" src="style/images/transfer.jpg"
+                         alt="Taxi4u - Airport transfers">
+                </div>
+                <div class="col-12 col-md-8 float-right p-2">
+                    <p class="card-text">Safe and trustworthy airport transfers arrivals and departures island wide
+                        24 hours professional service</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="excursion" class="container-fluid">
+    <div class="row">
+        <div class="services card border w-100">
+            <div class="card-header text-center text-light rounded-0" style="background-color: #1A1C7F">
+                <h4 class="card-title p-0 m-0">Excursions all over the island</h4>
+            </div>
+            <div class="card-body">
+                <div class="col-12 col-md-4 float-left p-2">
+                    <img class="img-fluid rounded border border-dark" src="style/images/excursions.jpg"
+                         alt="Taxi4u - Excursions all over the island">
+                </div>
+                <div class="col-12 col-md-8 float-left p-2">
+                    <p class="card-text">Adventurous tours island wide to ensure you a memorable stay, experience the
+                        beauty, taste the flavors and feel safe with us here on the island of Aphrodite, the island of
+                        love</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="contact" class="divide col-lg-12 px-1"></div>
+
+<div id="gallery" class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="services card border">
+                <div class="card-header text-center text-light rounded-0" style="background-color: #1A1C7F">
+                    <h4 class="card-title p-0 m-0">Gallery</h4>
+                </div>
+                <div class="card-body d-flex justify-content-start">
+                    <div class="row">
+                        <div class="col-6 col-md-2 p-2">
+                            <img class="img-fluid" src="viber/car/1.jpg"
+                                alt="Taxi4u - Taxi services">
+                        </div>
+                        <div class="col-6 col-md-2 p-2">
+                            <img class="img-fluid" src="viber/car/2.jpg"
+                                alt="Taxi4u - Taxi services">
+                        </div>
+                        <div class="col-6 col-md-2 p-2">
+                            <img class="img-fluid" src="viber/car/3.jpg"
+                                alt="Taxi4u - Taxi services">
+                        </div>
+                        <div class="col-6 col-md-2 p-2">
+                            <img class="img-fluid" src="viber/car/4.jpg"
+                                alt="Taxi4u - Taxi services">
+                        </div>
+                        <div class="col-6 col-md-2 p-2">
+                            <img class="img-fluid" src="viber/car/5.jpg"
+                                alt="Taxi4u - Taxi services">
+                        </div>
+                        <div class="col-6 col-md-2 p-2">
+                            <img class="img-fluid" src="viber/car/6.jpg"
+                                alt="Taxi4u - Taxi services">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="contact" class="divide col-lg-12 px-1"></div>
+
+<div class="pre-footer container-fluid">
+    <div class="row">
+        <div class="widget col-12 col-sm-12 col-md-4 col-lg-4">
+            <div class="card h-100">
+                <div class="card-header bg-danger text-light">Contact us</div>
+                <div class="card-body">
+                    <ul class="p-0">
+                        <li class="text-danger">
+                            <i class="fas fa-map-marker-alt"></i> Paphos, Cyprus
+                        </li>
+                        <li>
+                            <a href="tel:+35799030826">
+                                <i class="fa fa-phone-square fa-flip-horizontal" aria-hidden="true"></i> +35799030826
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/taxi4ucyprus" target="_blank">
+                                <i class="fab fa-facebook-square" aria-hidden="true"></i> fb.me/taxi4ucyprus
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.tripadvisor.com/Profile/Taxi4UCyprus" target="_blank">
+                                <i class="fab fa-tripadvisor"></i> tripadvisor.com/Profile/Taxi4UCyprus
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:office@taxi4u-cy.com" target="_blank">
+                                <i class="fa fa-envelope" aria-hidden="true"></i> office@taxi4u-cy.com
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="widget col-12 col-sm-12 col-md-4 col-lg-4">
+            <div class="card h-100">
+                <div class="card-header bg-danger text-light">Weather</div>
+                <div class="card-body">
+                    <div class="weather">
+                        <a class="weatherwidget-io" href="https://forecast7.com/en/34d7732d43/paphos/"
+                           data-label_1="Paphos" data-label_2="Cyprus" data-font="Open Sans" data-mode="Current"
+                           data-days="3" data-theme="pure" data-basecolor="rgba(255, 255, 255, 0)">Paphos Cyprus</a>
+                        <script>
+                            !function (d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (!d.getElementById(id)) {
+                                    js = d.createElement(s);
+                                    js.id = id;
+                                    js.src = 'https://weatherwidget.io/js/widget.min.js';
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                }
+                            }(document, 'script', 'weatherwidget-io-js');
+                        </script>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="widget col-12 col-sm-12 col-md-4 col-lg-4 text-center">
+            <div class="card h-100">
+                <div class="card-header bg-danger text-light">TripAdvisor</div>
+                <div class="card-body">
+                    <p class="card-text text-success">
+                        <!--                        <i class="fas fa-star"></i>-->
+                        <!--                        <i class="fas fa-star"></i>-->
+                        <!--                        <i class="fas fa-star"></i>-->
+                        <!--                        <i class="fas fa-star"></i>-->
+                        <!--                        <i class="fas fa-star-half-alt"></i>-->
+                    </p>
+                    <img src="style/images/tripadvisorlogo.png"
+                         class="img-fluid" style="max-width: 120px;"
+                         alt="Taxi4u - Tripadvisor's logo ">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="divide col-lg-12 py-1"></div>
+
+<div class="footer container-fluid">
+    <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 p-2">
+            <p class="text-md-center">
+                Copyright &copy;<a class="organization" href="https://www.taxi4u-cy.com">
+                    <b style="text-transform: uppercase">
+                        taxi4u-cy.com
+                    </b> </a> 2021
+            </p>
+
+            <p class="text-md-right">
+                Developed by
+                <a class="credits" href="https://www.linkedin.com/in/spasimirkirov/" target="_blank">Spasimir Kirov</a>
+            </p>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
